5536465762de02ef8b9bf73f91ad83ccddc5904b
APM-SYSTEM <APM-SYSTEM@DESKTOP-488FP15>
2019-07-21 13:26:52 +0530

    firest project

---
+++ src/test/java/Test/Demo.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("hiii",true);
+	}
+	
+
+}
